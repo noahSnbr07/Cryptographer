bf59b412c877a53860bd27995c75abff62be9e83
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2024-02-28 20:50:26 +0100

    minified some code and maed thies whole mess actually work by converting string to int after hours of painful debugging

--- src/pages/Output.jsx
+++ src/pages/Output.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom';
+import '../styles/output.css';
+import copyValue from '../utils/functions/copyValue';
+import encrypt from '../utils/functions/encrypt.js'
+import decrypt from '../utils/functions/decrypt.js'
 export default function Output() {
+    const [newData, setNewData] = useState();
+
+    const [urlParams] = useSearchParams({});
+
+    //params from the url usable
+    const [localParams] = useState({
+        function: urlParams.get('f'),
+        string: urlParams.get('s'),
+        key: urlParams.get('k'),
+    });
+
+    useEffect(() => {
+        if (localParams.function === 'encrypt') {
+            setNewData(encrypt(localParams.string, parseInt(localParams.key)));
+        } else if (localParams.function === 'decrypt') {
+            setNewData(decrypt(localParams.string, localParams.key));
+        }
+    }, [localParams]);
+
     return (
-        <div>Output</div>
-    )
+        <div className='output-page'>
+            <section className='output-value'>
+                <textarea readOnly={true} value={newData} />
+            </section>
+            <section className='output-information'>
+                <div className='info-key'>
+                    {localParams.key}
+                </div>
+                <Link className='info-link-1' to={'/new'}> Retry </Link>
+                <Link className='info-link-2' to={'/'}> Home </Link>
+                <button
+                    className='info-copy' onClick={() => {
+                        copyValue(newData);
+                    }}
+                > Copy </button>
+            </section>
+        </div>
+    );
 }
